bf7ab2f54c2b0451e070a981ad67da60099794ed
rsaez2 <68204864+rsaez2@users.noreply.github.com>
2020-07-12 21:05:59 -0400

    Added to todo.js
    Changed the array to a constant, added ul, made a template using map. Hopefully, if it's correct, it will create a list.

--- todo.js
+++ todo.js
@@ -1,9 +1,17 @@
 let todo = "";
-let todoarry = []
+const todoarrys = []
 const todoadd = document.querySelector("#newTodo")
+const ul = document.querySelector("ul")
 
 document.querySelector('form').addEventListener("submit",function(){
     event.preventDefault();
    
     todo = todoadd.value;
+    todoarrys.push(todo)
+    const list = todoarrys.map(todoarry => `
+    <li>
+    <p> ${todoarry}</p>
+    </li>
+    `);
+    ul.innerHTML = list.join('');
 })
